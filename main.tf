c1e231af14e69d42c263f06e32ac7024a7d2ae47
Md. Naser Arafat <159879031+griffinofblue@users.noreply.github.com>
2024-09-21 13:14:49 +0600

    include security group - edition

--- main.tf
+++ main.tf
@@ -58,8 +58,8 @@ resource "aws_security_group_rule" "sg_blog_https_in" {
 
 resource "aws_security_group_rule" "sg_blog_everything_out" {
   type        = "egress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 0
+  to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 

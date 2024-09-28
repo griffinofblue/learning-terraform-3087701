ec855fd260bcde437301d96ed1d51cb5864fefbf
Md. Naser Arafat <159879031+griffinofblue@users.noreply.github.com>
2024-09-28 19:42:57 +0600

    lb downgrade

--- main.tf
+++ main.tf
@@ -43,29 +43,39 @@ resource "aws_instance" "blog" {
 }
 
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 5.0"
+
+  name = "blog-alb"
 
   load_balancer_type = "application"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-  listeners = [ 
+  target_groups = [
     {
-      port     = 80
-      protocol = "HTTP"
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = [
+        {
+          target_id = aws_instance.blog.id
+          port = 80
+        },
+      ]
     }
   ]
 
-  target_groups = {
-    name_prefix      = "blog"
-    protocol         = "HTTP"
-    port             = 80
-    target_type      = "instance"
-    target_id        = aws_instance.blog.id
-  }
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
